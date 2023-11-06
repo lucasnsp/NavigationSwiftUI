a3f20e45796baf54de1aefb700e7c48202645596
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-06 18:27:34 -0300

    Add files via upload

---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Lucas Neves dos santos pompeu on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
